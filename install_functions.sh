68aa89c07c675dcd5fddcc289cf985109c92a610
Kristofer Rye <kristofer.rye@gmail.com>
2013-06-08 00:59:48 -0400

    merge functions.sh to generate.sh, for portability

--- install_functions.sh
+++ install_functions.sh
@@ -2,9 +2,8 @@
 
 uninstall ()
 {
-	if test -e /home/$USER/.bin/.mfjen;
+	if test -e /home/$USER/.bin/mfjen;
 	then
-		rm -rf /home/$USER/.bin/.mfjen
 		rm /home/$USER/.bin/mfjen
 
 		echo "Should be gone now!"
@@ -26,9 +25,6 @@ install ()
 	else
 		echo "Path is already set!"
 	fi
-	
-	mkdir -p /home/$USER/.bin/.mfjen
-	cp functions.sh /home/$USER/.bin/.mfjen
-	cp generate.sh /home/$USER/.bin/.mfjen
-	ln -s /home/$USER/.bin/.mfjen/generate.sh /home/$USER/.bin/mfjen
+
+	ln -s `pwd`/generate.sh /home/$USER/.bin/mfjen
 }
